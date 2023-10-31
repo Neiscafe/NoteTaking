019a0840c0a6d8014f71a0a37119b1c8ebaeb7bb
Gustavo Heck <compumaxsistemas@outlook.com>
2023-10-31 17:59:54 -0300

    Tentativas de funcionamento

--- Viewmodels/MainViewModel.cs
+++ Viewmodels/MainViewModel.cs
@@ -2,18 +2,22 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NoteTaking.Viewmodels
 {
-    public class MainViewModel
+    public class MainViewModel: INotifyPropertyChanged
     {
         public MainViewModel()
         {
+            this.Boards = UnitOfWork.BoardRepository.GetBoards();
         }
 
-        public ObservableCollection<Board> Boards { get; set; } = UnitOfWork.BoardRepository.GetBoards();
+        public ObservableCollection<Board> Boards { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
--- Views/BoardUserControl.xaml.cs
+++ Views/BoardUserControl.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using NoteTaking.Models;
 using NoteTaking.Viewmodels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NoteTaking.Views
 {
@@ -28,7 +16,7 @@ namespace NoteTaking.Views
             DataContext = new BoardViewModel(CurrentBoard);
         }
         public static readonly DependencyProperty BoardProperty = 
-            DependencyProperty.Register("Content", typeof(Board), typeof(BoardUserControl), new PropertyMetadata(new Board()));
+            DependencyProperty.Register("Content", typeof(Board), typeof(BoardUserControl), new PropertyMetadata(new Board(1 ,"teste")));
 
         public Board CurrentBoard
         {
